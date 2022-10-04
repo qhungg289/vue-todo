928a0411ea1266e1bdd554a0d64ecd03b4029349
Hung Duong Quang <qhungg289@protonmail.com>
2022-10-04 10:24:50 +0700

    Add two button components

---
+++ src/components/Button/ButtonPrimary.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    onClick: Function
+})
+</script>
+
+<template>
+    <button @click="onClick" class="px-6 py-2 rounded-lg bg-emerald-600 font-medium text-zinc-50 hover:bg-emerald-700">
+        <slot></slot>
+    </button>
+</template>
